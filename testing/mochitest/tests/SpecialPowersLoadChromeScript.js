48c19c2d2cfe9f2841c2afc4d9fef1b01592f6b1
Alexandre Poirot <poirot.alex@gmail.com>
2014-01-22 04:14:12 +0100

    Bug 965257 - Expose Assert.jsm in mochitest-plain chrome script r=ted

--- testing/mochitest/tests/SpecialPowersLoadChromeScript.js
+++ testing/mochitest/tests/SpecialPowersLoadChromeScript.js
@@ -4,3 +4,8 @@ addMessageListener("foo", function (message) {
   sendAsyncMessage("bar", message);
 });
 
+addMessageListener("assert", function (message) {
+  assert.ok(true, "valid assertion");
+  assert.ok(false, "invalid assertion");
+  assert.equal(1, 2, "invalid equal assertion");
+});
